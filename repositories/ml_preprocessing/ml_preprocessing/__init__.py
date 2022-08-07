4f199609115705c701c43c2b4336c42d816df585
fjmoronreyes <78026047+fjmoronreyes@users.noreply.github.com>
2022-08-07 12:59:18 +0200

    Add paragraphs_generator

--- repositories/ml_preprocessing/ml_preprocessing/__init__.py
+++ repositories/ml_preprocessing/ml_preprocessing/__init__.py
@@ -8,5 +8,6 @@ FILE_PATH = os.path.dirname(__file__)
 STOPWORDS = os.environ.get("STOPWORDS", f"{FILE_PATH}/stopwords")
 
 DATASETS = os.environ.get("DATASETS", f"{FILE_PATH}/../../datasets/raw_files")
-SENTENCES_FILES = os.environ.get("DATASETS", f"{FILE_PATH}/../../datasets/sentences_files")
-SEQUENCES_FILES = os.environ.get("DATASETS", f"{FILE_PATH}/../../datasets/sequences_files")
+SENTENCES_FILES = os.environ.get("SENTENCES_FILES", f"{FILE_PATH}/../../datasets/sentences_files")
+SEQUENCES_FILES = os.environ.get("SEQUENCES_FILES", f"{FILE_PATH}/../../datasets/sequences_files")
+PARAGRAPHS_FILES = os.environ.get("PARAGRAPHS_FILES", f"{FILE_PATH}/../../datasets/paragraphs_files")
