7e9e65c69f471800001464525424abc82fd8eba0
fjmoronreyes <78026047+fjmoronreyes@users.noreply.github.com>
2022-08-07 11:23:32 +0200

    Add relative paths to ml_preprocessing module

--- repositories/ml_preprocessing/ml_preprocessing/testing.py
+++ repositories/ml_preprocessing/ml_preprocessing/testing.py
@@ -6,28 +6,5 @@ import yaml
 from ml_preprocessing.cleaner import Cleaner
 from sequences_generator import SequencesGenerator
 
-from ml_preprocessing import es_stopwords
-
-
-################
-# test cleaner #
-################
-"""
-cleaner = Cleaner()
-with open(
-    "/Users/fjmoronreyes/text_generation/repositories/datasets/raw_files/jrr_tolkien-la-ultima-cancion-de-bilbo.txt"
-) as f:
-    content = f.read()
-
-# file_sentences = nltk.tokenize.sent_tokenize(content)
-file_cleaned = cleaner.make_clean(content, stopwords=False)
-print(file_cleaned)
-"""
-
-############################
-# test sequences_generator #
-############################
-
-path = "/Users/fjmoronreyes/text_generation/repositories/datasets/"
-sequences = SequencesGenerator(path, stopwords=False)
-sequences.create_sentences_files()
+from ml_preprocessing import STOPWORDS
+from ml_preprocessing import DATASETS
